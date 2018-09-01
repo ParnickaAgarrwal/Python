74a334b7e4df1e792e8cd2348657ef263d1f5d93
ParnickaAgarrwal <parnickaagarrwal08@gmail.com>
2018-09-01 13:33:44 +0530

    Done

--- assignment 12.py
+++ assignment 12.py
@@ -13,9 +13,9 @@ webbrowser.open_new_tab('http://www.google.com/search?btnG=1&q=%s' % search)
 
 #Q.3- Write a program to rename all the files in a directory and convert them into .jpg file format.
 import os
-path = r'''C:/Users/Asus/Desktop/Python/New_Directory'''
-files = os.listdir(path)
-i = 1
-for f in files:
+path=r'''C:/Users/Asus/Desktop/Python/New_Directory'''
+file=os.listdir(path)
+i=1
+for f in file:
     os.rename(os.path.join(path, f), os.path.join(path, str(i)+'.jpg'))
-    i += 1
+    i+=1
